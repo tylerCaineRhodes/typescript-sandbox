b1389f55cc473db088c2406e26737c71e590a193
Tyler Rhodes <tyler.rhodes@yale.edu>
2020-08-12 14:10:31 -0500

    Refactor sorter to abstract class

--- sort/src/Sorter.ts
+++ sort/src/Sorter.ts
@@ -1,19 +1,15 @@
-interface Sortable {
-  length: number;
-  compare(i: number, j: number): boolean;
-  swap(i: number, j: number): void;
-}
-
-export class Sorter {
-  constructor(public collection: Sortable) {}
+export abstract class Sorter {
+  abstract compare(i: number, j: number): boolean;
+  abstract swap(i: number, j: number): void;
+  abstract length: number
 
   sort(): void {
-    const { length } = this.collection;
+    const { length } = this;
 
     for (let i = 0; i < length; i++) {
       for (let j = 0; j < length - i - 1; j++) {
-        if(this.collection.compare(j, j + 1)){
-          this.collection.swap(j, j + 1)
+        if(this.compare(j, j + 1)){
+          this.swap(j, j + 1)
         }
       }
     }
--- sort/src/index.ts
+++ sort/src/index.ts
@@ -3,24 +3,20 @@ import { NumbersCollection } from './NumbersCollection';
 import {CharactersCollection} from './CharactersCollection'
 import { LinkedList } from './LinkedList'
 
-const numbersCollection = new NumbersCollection([10, 3, -5, 0])
+const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
+
 const charactersCollection = new CharactersCollection('Xaaa');
+
 const linkedListCollection = new LinkedList();
   linkedListCollection.add(500)
   linkedListCollection.add(-10)
   linkedListCollection.add(-3)
   linkedListCollection.add(4)
 
-const sorter = new Sorter(numbersCollection);
-const sorterChar = new Sorter(charactersCollection);
-const sorterLL = new Sorter(linkedListCollection);
+charactersCollection.sort();
+linkedListCollection.sort()
+numbersCollection.sort();
 
-sorter.sort();
-sorterChar.sort();
-sorterLL.sort();
-
-linkedListCollection.print();
-
-console.log(numbersCollection.data)
 console.log(charactersCollection.data)
-
+console.log(numbersCollection.data)
+linkedListCollection.print();
